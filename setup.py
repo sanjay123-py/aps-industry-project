ddf91cb52cadcb1e1db211ca5f4e3c860a70b2cd
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-07 22:21:09 +0530

    data ingestion trail1

--- setup.py
+++ setup.py
@@ -2,19 +2,24 @@ from setuptools import find_packages,setup
 from typing import List
 
 REQUIREMENTS_FILENAME = 'requirements.txt'
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
-    with open(REQUIREMENTS_FILENAME) as f:
-        requirements_list = [filenames.replace('\n',"") for filenames in f.readlines()]
-
-
+    
+    with open(REQUIREMENTS_FILENAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
 
 
 setup(
     name = 'sensor',
     version = "1.0",
     author = "sanjay",
-    author_emial = "sanjaysanjay1270@gmail.com",
-    pakages = find_packages(),
+    author_email = "sanjaysanjay1270@gmail.com",
+    packages = find_packages(),
     install_requires = get_requirements(),
 )
