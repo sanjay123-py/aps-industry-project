5e52c472f50130858394cb8861056262c5ce826b
snajay123-py <sanjaysanjay1270@gmail.com>
2022-12-08 15:10:44 +0530

    data ingestion trail3

---
+++ sensor/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+#each logging file should be save according to the timing
+LOG_FILE_NAME=f"{datetime.now().strftime('%m%d%Y__%H:%M:%S')}.log"
+
+#creating logging directory path
+LOG_FILE_DIR=os.path.join(os.getcwd(),'logs')
+
+#creating a directory if not exists for storing logging informations
+os.makedirs(LOG_FILE_DIR,exist_ok=True)
+
+#Creating a logging path
+LOG_FILE_PATH=os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+#configure logging 
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    
+
+)
