959826eebe8dc55d741c33c37bf692024c54dd3c
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-09 11:11:35 +0530

    data ingestion trial6

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import pymongo
+from sensor.exception import SensorException
+from sensor.logger import logging
+import os, sys
+def test_logger_and_exception():
+     try:
+          result=1/0
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+     except Exception as e:
+          raise  SensorException(e, sys)
+if __name__ == '__main__':
+     try:
+          test_logger_and_exception()
 
-# Database Name
-dataBase = client["neurolabDB"]
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+     except Exception as e:
+          print(e)
+     
+          
