8b1bad02bc6c7dba348ddca70b9dca38b4acf630
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-09 20:03:00 +0530

    data ingestion trial56.3

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,5 +1,31 @@
+import os, sys,datetime.datetime
+from sensor.exception import SensorException
+from sensor.logger import logging
+FILE_NAME = "sensor.csv"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+TRANSFORMER_OBJECT_FILE_NAME = "transformer.pkl"
+TARGET_ENCODER_OBJECT_FILE_NAME = "target_encoder.pkl"
+MODEL_FILE_NAME = "model.pkl"
 
-class DataIngestionConfig:...
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        self.artifact_dir=os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H:%M:%S')}")
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig()):
+        try:
+            self.database_name="aps"
+            self.collection_name="sensor"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+            self.test_size = 0.2
+        except Exception as e:
+            logging.debug(str(e))
+            raise SensorException(e, sys)
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
