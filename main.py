894b04cf1324a22e2b88b23833dd473925232c6b
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-09 12:18:12 +0530

    data_ingestion trial6.1

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os, sys
-def test_logger_and_exception():
-     try:
-          result=1/0
-
-     except Exception as e:
-          raise  SensorException(e, sys)
+# def test_logger_and_exception():
+#      try:
+#           logging.info("Starting the test_logger_and_exception")
+#           result=1/0
+#           print(result)
+#           logging.info("Stopping the test_logger_and_exception")
+#      except Exception as e:
+#           logging.debug(str(e))
+#           raise  SensorException(e, sys)
 if __name__ == '__main__':
-     try:
-          test_logger_and_exception()
+     try:...
 
      except Exception as e:
           print(e)
