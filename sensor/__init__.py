7684091bc1a53c96b560dc3d287f934f8aa3f4d0
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-13 12:17:02 +0530

    data validation trial

--- sensor/__init__.py
+++ sensor/__init__.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
+from sensor.logger import logging
 
+logging.info("Loading environment variable from .env file")
 print(f"Loading environment variable from .env file")
 
 load_dotenv()
---
+++ sensor/components/data_validation.py
@@ -0,0 +1,1 @@
+from sensor.logger import logging
