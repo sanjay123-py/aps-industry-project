615916591c0958d332f076181c4d41a83293f78e
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-25 01:26:41 +0530

    data transformation completed

--- sensor/config.py
+++ sensor/config.py
@@ -7,5 +7,11 @@ import os, sys
 class EnvironmentVariables:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
 
+TARGET_COLUMN_MAPPING={
+    'pos':1,
+    'neg':0
+}
+
 env_variable = EnvironmentVariables()
 client = pymongo.MongoClient(env_variable.mongo_db_url)
+TARGET_COLUMN = 'class'
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -4,6 +4,7 @@ from sensor.logger import logging
 from  sensor.exception import SensorException
 import yaml
 import os, sys
+import dill
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     try:
@@ -35,6 +36,52 @@ def convert_columns_float(df:pd.DataFrame, excluded_columns:list)->pd.DataFrame:
         if(df_column not in excluded_columns):
             df[df_column]=df[df_column].astype(float)
     return df
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of utils")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+        logging.info("Exited the save_object method of utils")
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys) 
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise SensorException(e, sys)
+
 if __name__ == '__main__':
     try:
         get_collection_as_dataframe(database_name="aps", collection_name="sensor")
