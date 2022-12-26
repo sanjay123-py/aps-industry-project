0c882472db5d52fe987366bd83e8e07978f6cdab
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-26 20:22:29 +0530

    model evaluation trial1

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -53,5 +53,7 @@ class ModelTrainerConfig:
         self.model_path = os.path.join(self.model_trainer_dir,"model",MODEL_FILE_NAME)
         self.expected_score = 0.7
         self.overfitting_threshold = 0.1
-class ModelEvaluationConfig:...
+class ModelEvaluationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.change_threshold = 0.01    
 class ModelPusherConfig:...
