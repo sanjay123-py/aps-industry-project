c66ddfed3c2b0a214cbbc74c109b13dbf21cae0f
sanjay123-py <sanjaysanjay1270@gmail.com>
2022-12-31 15:05:59 +0530

    testing batch prediction

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from sensor.pipelines.training_pipeline import start_training_pipeline
+from sensor.pipelines.batch_prediction import start_batch_prediction
+
 if  __name__ == '__main__':
      try:
-          start_training_pipeline()
+          # start_training_pipeline()
+          output_file = start_batch_prediction('/config/workspace/aps_failure_training_set1.csv')
+          print(output_file)
      except Exception as e:
           print(e)
